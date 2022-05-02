3c1691a0d35164354df9801f040a98a862e47981
Arturs <arturs@Arturss-Air.broadband>
2022-05-02 10:24:46 +0100

    first-commit

--- Components/Nav.js
+++ Components/Nav.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Nav = () => {
   return (
-    <div className='bg-gradient-to-r from-bg_custom_3 to-bg_custom_2 ... h-20 fixed w-full z-50'>Nav</div>
+    <div className='bg-gradient-to-r from-bg_custom_3 to-bg_custom_2 ... sticky w-full z-50'>Nav</div>
   )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,11 @@ import Image from 'next/image'
 
 const Home = () => {
   return (
-    <div className='relative top-14 bg-gradient-to-r from-bg_custom_3 to-bg_custom_2 ...'>
+    <div className='relative bg-gradient-to-r from-bg_custom_3 to-bg_custom_2 ...'>
     <div className="h-screen flex content-center justify-center">
-    <div className='text-12xl relative left-5 top-44 -z-2'><h1>ART &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;LIVE</h1></div>
-      <div className='text-7xl font-bold w-288 h-288 pt-20 absolute z-40'>
+    <div className='text-12xl relative left-5 top-52 -z-2'><h1>ART &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;LIVE</h1></div>
+      
+      <div className='text-7xl font-bold w-288 h-288 pt-28 absolute z-40'>
         <Image
           src="https://res.cloudinary.com/dyvgcv5se/image/upload/v1651424117/AI/AdobeStock_485407292_Converted_nhovmm.svg"
           alt="logo"
