f6cb70772964879cfe05e891a27abe6a2da2d7eb
Arturs <arturs@Arturss-Air.broadband>
2022-05-02 12:47:34 +0100

    first-commit

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,33 @@
 import Image from 'next/image'
 
+// const Home = () => {
+//   return (
+//     <div className='relative bg-gradient-to-r from-bg_custom_3 to-bg_custom_2 ...'>
+//     <div className="h-screen flex content-center justify-center">
+//     <div className='text-12xl relative left-4 top-52 -z-2'><h1>ART &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;LIVE</h1></div>
+      
+//       <div className='text-7xl font-bold w-288 h-288 pt-28 absolute z-40'>
+//         <Image
+//           src="https://res.cloudinary.com/dyvgcv5se/image/upload/v1651424117/AI/AdobeStock_485407292_Converted_nhovmm.svg"
+//           alt="logo"
+//           width={1041}
+//           height={588}
+//           quality = {100}
+//           layout = 'responsive'
+//         />
+//       </div>
+//     </div>
+//     <div className="h-screen">Hi</div>
+//     </div>
+    
+//   )
+// }
+
+// export default Home
+
 const Home = () => {
   return (
-    <div className='relative bg-gradient-to-r from-bg_custom_3 to-bg_custom_2 ...'>
-    <div className="h-screen flex content-center justify-center">
-    <div className='text-12xl relative left-4 top-52 -z-2'><h1>ART &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;LIVE</h1></div>
-      
-      <div className='text-7xl font-bold w-288 h-288 pt-28 absolute z-40'>
-        <Image
-          src="https://res.cloudinary.com/dyvgcv5se/image/upload/v1651424117/AI/AdobeStock_485407292_Converted_nhovmm.svg"
-          alt="logo"
-          width={1041}
-          height={588}
-          quality = {100}
-          layout = 'responsive'
-        />
-      </div>
-    </div>
-    <div className="h-screen">Hi</div>
-    </div>
+   <div>Welcome</div>
     
   )
 }
