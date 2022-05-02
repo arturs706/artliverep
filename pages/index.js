d964db7af08fbae018e5ff03d575c2a62909f87d
Arturs <arturs@Arturss-Air.broadband>
2022-05-02 10:11:58 +0100

    first-commit

--- pages/index.js
+++ pages/index.js
@@ -15,40 +15,6 @@ const Home = () => {
           layout = 'responsive'
         />
       </div>
-      <div className='opacity-70 absolute left-20 top-152 w-32 flex justify-between'>
-      <Image
-        src="https://res.cloudinary.com/dyvgcv5se/image/upload/v1651442929/icons_3/104458_facebook_social_media_fb_social_icon_zwmixv.svg"
-        alt="icon"
-        width={24}
-        height={24}
-        quality = {100}
-        layout = 'fixed'
-      />
-      <Image
-        src="https://res.cloudinary.com/dyvgcv5se/image/upload/v1651442929/icons_3/843773_whatsapp_icon_eizcxp.svg"
-        alt="icon"
-        width={24}
-        height={24}
-        quality = {100}
-        layout = 'fixed'
-      />
-      <Image
-        src="https://res.cloudinary.com/dyvgcv5se/image/upload/v1651442929/icons_3/394187_github_icon_v0z0y3.svg"
-        alt="icon"
-        width={24}
-        height={24}
-        quality = {100}
-        layout = 'fixed'
-      />
-      <Image
-        src="https://res.cloudinary.com/dyvgcv5se/image/upload/v1651442929/icons_3/1161953_instagram_icon_phevm7.svg"
-        alt="icon"
-        width={24}
-        height={24}
-        quality = {100}
-        layout = 'fixed'
-      />
-    </div>
     </div>
     <div className="h-screen">Hi</div>
     </div>
