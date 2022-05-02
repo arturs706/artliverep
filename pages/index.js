b36493423b333904eb90e5c487ca83d0b0742698
Arturs <arturs@Arturss-Air.broadband>
2022-05-02 10:08:47 +0100

    first-commit

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,59 @@
-export default function Home() {
+import Image from 'next/image'
+
+const Home = () => {
   return (
-    <h1 className="text-3xl font-bold underline w-full h-screen bg-indigo-500">
-      Hello world!
-    </h1>
+    <div className='relative top-14 bg-gradient-to-r from-bg_custom_3 to-bg_custom_2 ...'>
+    <div className="h-screen flex content-center justify-center">
+    <div className='text-12xl relative left-5 top-44 -z-2'><h1>ART &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;LIVE</h1></div>
+      <div className='text-7xl font-bold w-288 h-288 pt-20 absolute z-40'>
+        <Image
+          src="https://res.cloudinary.com/dyvgcv5se/image/upload/v1651424117/AI/AdobeStock_485407292_Converted_nhovmm.svg"
+          alt="logo"
+          width={1041}
+          height={588}
+          quality = {100}
+          layout = 'responsive'
+        />
+      </div>
+      <div className='opacity-70 absolute left-20 top-152 w-32 flex justify-between'>
+      <Image
+        src="https://res.cloudinary.com/dyvgcv5se/image/upload/v1651442929/icons_3/104458_facebook_social_media_fb_social_icon_zwmixv.svg"
+        alt="icon"
+        width={24}
+        height={24}
+        quality = {100}
+        layout = 'fixed'
+      />
+      <Image
+        src="https://res.cloudinary.com/dyvgcv5se/image/upload/v1651442929/icons_3/843773_whatsapp_icon_eizcxp.svg"
+        alt="icon"
+        width={24}
+        height={24}
+        quality = {100}
+        layout = 'fixed'
+      />
+      <Image
+        src="https://res.cloudinary.com/dyvgcv5se/image/upload/v1651442929/icons_3/394187_github_icon_v0z0y3.svg"
+        alt="icon"
+        width={24}
+        height={24}
+        quality = {100}
+        layout = 'fixed'
+      />
+      <Image
+        src="https://res.cloudinary.com/dyvgcv5se/image/upload/v1651442929/icons_3/1161953_instagram_icon_phevm7.svg"
+        alt="icon"
+        width={24}
+        height={24}
+        quality = {100}
+        layout = 'fixed'
+      />
+    </div>
+    </div>
+    <div className="h-screen">Hi</div>
+    </div>
+    
   )
 }
+
+export default Home
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,38 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'bg_custom_1': '#1F2021',
+        'bg_custom_2': '#161716',
+        'bg_custom_3': '#2d2d2d',
+        'bg_custom_4': '#555555',
+        'bg_blue_custom_1': '#183563',
+        'bg_blue_custom_2': '#040d1b',
+        'bg_blue_custom_3': '#14357b',
+        'bg_blue_custom_4': '#132355',
+        'purple_custome_1:': '#804a87',
+        'purple_custome_2': '#e861e1',
+        'blue_new_1': '#3500D3',
+        'font_color': '#bfc0bf',
+      },
+      height: {
+        '288': '72rem',
+        '144': '36rem',
+      },
+      width: {
+        '288': '72rem',
+        '144': '36rem',
+      },
+      fontSize: {
+        '10xl': '10rem',
+        '12xl': '12rem',
+      },
+      spacing: {
+        '144': '36rem',
+        '152': '40.5rem',
+      }
+    },
   },
   plugins: [],
 }
