0fbb18fac7dbd88a06f4aea02d0e7145db40b4ff
Arturs <arturs@Arturss-Air.broadband>
2022-05-02 09:39:33 +0100

    first-commit

--- Components/Nav.js
+++ Components/Nav.js
@@ -1,11 +1,11 @@
 import React, {useEffect, useRef} from 'react'
 import gsap from 'https://cdn.skypack.dev/gsap'
 import ScrollTrigger from "https://cdn.skypack.dev/gsap/ScrollTrigger";
-import { useSession, signIn, signOut } from "next-auth/react"
+// import { useSession, signIn, signOut } from "next-auth/react"
 import Image from 'next/image'
 
 const Nav = () => {
-  const { data: session } = useSession()  
+//   const { data: session } = useSession()  
   
   useEffect (() => {
     gsap.registerPlugin(ScrollTrigger);
@@ -35,7 +35,7 @@ const Nav = () => {
           <p className='text-sm pr-12'>CONTACT</p>
         </div>
         <div>
-        { session 
+        {/* { session 
         ? 
         <div className='pr-14'>
           
@@ -55,7 +55,7 @@ const Nav = () => {
         <div><button className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 "onClick={() => signIn()}>SIGN IN</button></div>
         </div> 
         
-        }
+        } */}
         </div>
         
         </div>
