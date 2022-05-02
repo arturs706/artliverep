2553f6b6833b074c930eee1cddd7c11e810c4a44
Arturs <arturs@Arturss-Air.broadband>
2022-05-02 13:15:43 +0100

    first-commit

--- Components/Nav.js
+++ Components/Nav.js
@@ -5,9 +5,9 @@ import Image from 'next/image'
 const Nav = () => {
     // const { data: session } = useSession()  
   return (
-    <div className='h-24 flex bg-gray-800 justify-between items-center px-16'>
-        <div>ART.LIVE</div>
-        <div>Login</div>
+    <div className='h-24 bg-gray-800 flex justify-between items-center px-16'>
+        <div className='bg-gray-800'>ART.LIVE</div>
+        <div className='bg-gray-800'>Login</div>
     </div>
   )
 }
